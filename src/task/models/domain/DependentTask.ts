5c3f17e956b0d291424a8d4435eb20d0d2d104e9
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-08-02 01:02:29 +0200

    feat(task): add DependentTask

---
+++ src/task/models/domain/DependentTask.ts
@@ -0,0 +1,6 @@
+import { Task } from './Task';
+
+export interface DependentTask<TKind, TArgs extends unknown[], TReturn>
+  extends Task<TKind, TArgs, TReturn> {
+  readonly dependencies: DependentTask<TKind, TArgs, TReturn>;
+}
