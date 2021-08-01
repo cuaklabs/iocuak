9866e0fa959b0d2d2b9a551f137ac8c5808c1e58
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-08-02 00:41:31 +0200

    feat(task): update Task.kind to be readonly

--- src/task/models/domain/Task.ts
+++ src/task/models/domain/Task.ts
@@ -1,7 +1,7 @@
 import { TaskStatus } from './TaskStatus';
 
 export interface Task<TKind, TArgs extends unknown[], TReturn> {
-  kind: TKind;
+  readonly kind: TKind;
   status: TaskStatus;
   perform(...args: TArgs): TReturn;
 }
