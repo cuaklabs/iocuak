496a49d776485e4132632280723fd9f38341e100
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-08-07 10:01:34 +0200

    feat: add index

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { DependentTaskBuilder } from './task/modules/DependentTaskBuilder';
+import { DependentTaskRunner } from './task/modules/DependentTaskRunner';
+import { TaskDependencyEngine } from './task/modules/TaskDependencyEngine';
+
+export { DependentTaskBuilder, DependentTaskRunner, TaskDependencyEngine };
