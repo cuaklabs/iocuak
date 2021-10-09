9edad0e41c4535d2e631ccd7dcfc8893f91cd67f
notaphplover <roberto.pintos.lopez@gmail.com>
2021-10-09 17:46:15 +0200

    feat(cuaktask): update index to expose Task

--- packages/cuaktask/index.ts
+++ packages/cuaktask/index.ts
@@ -1,6 +1,7 @@
 import { SetLike } from './common/modules/SetLike';
 import { BaseDependentTask } from './task/models/domain/BaseDependentTask';
 import { BaseTask } from './task/models/domain/BaseTask';
+import { Task } from './task/models/domain/Task';
 import { DependentTaskBuilder } from './task/modules/DependentTaskBuilder';
 import { DependentTaskRunner } from './task/modules/DependentTaskRunner';
 import { TaskDependencyEngine } from './task/modules/TaskDependencyEngine';
@@ -11,5 +12,6 @@ export {
   DependentTaskBuilder,
   DependentTaskRunner,
   SetLike,
+  Task,
   TaskDependencyEngine,
 };
