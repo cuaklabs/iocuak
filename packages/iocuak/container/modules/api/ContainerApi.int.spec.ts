58d0de293928eb536b170cbde8bdaa7de074d847
notaphplover <roberto.pintos.lopez@gmail.com>
2021-10-25 21:05:56 +0200

    ✨: Add ContainerApi module

---
+++ packages/iocuak/container/modules/api/ContainerApi.int.spec.ts
@@ -0,0 +1,225 @@
+import 'reflect-metadata';
+
+import { inject } from '../../../metadata/decorators/inject';
+import { injectable } from '../../../metadata/decorators/injectable';
+import { Newable } from '../../../task/models/domain/Newable';
+import { ServiceId } from '../../../task/models/domain/ServiceId';
+import { ContainerApi } from './ContainerApi';
+
+describe(ContainerApi.name, () => {
+  describe('.bind()', () => {
+    describe('having a type with no metadata', () => {
+      let typeFixture: Newable;
+
+      beforeAll(() => {
+        typeFixture = class {};
+      });
+
+      describe('when called', () => {
+        let containerApi: ContainerApi;
+
+        let result: unknown;
+
+        beforeAll(() => {
+          containerApi = new ContainerApi();
+
+          try {
+            containerApi.bind(typeFixture);
+          } catch (error: unknown) {
+            result = error;
+          }
+        });
+
+        it('should throw an Error', () => {
+          expect(result).toBeInstanceOf(Error);
+        });
+      });
+    });
+
+    describe('having a type with binding metadata and no constructor metadata nor properties metadata', () => {
+      let typeFixture: Newable;
+
+      beforeAll(() => {
+        @injectable()
+        class TypeFixture {
+          public property!: string;
+
+          constructor(public readonly parameter: string) {}
+        }
+
+        typeFixture = TypeFixture;
+      });
+
+      describe('when called', () => {
+        let containerApi: ContainerApi;
+
+        beforeAll(() => {
+          containerApi = new ContainerApi();
+
+          containerApi.bind(typeFixture);
+        });
+
+        describe('when called .get()', () => {
+          let result: unknown;
+
+          beforeAll(() => {
+            result = containerApi.get(typeFixture);
+          });
+
+          it('should return an instance with no properties set', () => {
+            expect(result).toBeInstanceOf(typeFixture);
+            expect(result).not.toHaveProperty('property');
+          });
+        });
+      });
+    });
+
+    describe('having a type with binding metadata and constructor metadata and no properties metadata', () => {
+      let parameterServiceIdFixture: ServiceId;
+      let typeFixture: Newable;
+
+      beforeAll(() => {
+        parameterServiceIdFixture = 'sample-service';
+
+        @injectable()
+        class TypeFixture {
+          public property: string | undefined;
+
+          constructor(
+            @inject(parameterServiceIdFixture)
+            public readonly parameter: unknown,
+          ) {}
+        }
+
+        typeFixture = TypeFixture;
+      });
+
+      describe('when called', () => {
+        let containerApi: ContainerApi;
+
+        beforeAll(() => {
+          containerApi = new ContainerApi();
+
+          containerApi.bind(typeFixture);
+        });
+
+        describe('when called .bind() with dependent service', () => {
+          let dependentTypeFixture: Newable;
+
+          beforeAll(() => {
+            @injectable({
+              id: parameterServiceIdFixture,
+            })
+            class DependentTypeFixture {}
+
+            dependentTypeFixture = DependentTypeFixture;
+
+            containerApi.bind(dependentTypeFixture);
+          });
+
+          describe('when called .get()', () => {
+            let result: unknown;
+
+            beforeAll(() => {
+              result = containerApi.get(typeFixture);
+            });
+
+            it('should return an instance', () => {
+              expect(result).toStrictEqual(
+                new typeFixture(new dependentTypeFixture()),
+              );
+            });
+          });
+        });
+
+        describe('when called .get()', () => {
+          let result: unknown;
+
+          beforeAll(() => {
+            result = containerApi.get(typeFixture);
+          });
+
+          it('should return an instance', () => {
+            expect(result).toBeInstanceOf(typeFixture);
+          });
+        });
+      });
+    });
+
+    describe('having a type with binding metadata and constructor metadata and properties metadata', () => {
+      let parameterServiceIdFixture: ServiceId;
+      let propertyServiceIdFixture: ServiceId;
+      let typeFixture: Newable;
+
+      beforeAll(() => {
+        parameterServiceIdFixture = 'sample-parameter-service';
+        propertyServiceIdFixture = 'sample-property-service';
+
+        @injectable()
+        class TypeFixture {
+          @inject(propertyServiceIdFixture)
+          public property: unknown;
+
+          constructor(
+            @inject(parameterServiceIdFixture)
+            public readonly parameter: unknown,
+          ) {}
+        }
+
+        typeFixture = TypeFixture;
+      });
+
+      describe('when called', () => {
+        let containerApi: ContainerApi;
+
+        beforeAll(() => {
+          containerApi = new ContainerApi();
+
+          containerApi.bind(typeFixture);
+        });
+
+        describe('when called .bind() on type dependencies', () => {
+          let parameterTypeFixture: Newable;
+          let propertyTypeFixture: Newable;
+
+          beforeAll(() => {
+            @injectable({
+              id: parameterServiceIdFixture,
+            })
+            class ParameterTypeFixture {}
+
+            @injectable({
+              id: propertyServiceIdFixture,
+            })
+            class PropertyTypeFixture {}
+
+            parameterTypeFixture = ParameterTypeFixture;
+            propertyTypeFixture = PropertyTypeFixture;
+
+            containerApi.bind(parameterTypeFixture);
+            containerApi.bind(propertyTypeFixture);
+          });
+
+          describe('when called .get()', () => {
+            let result: unknown;
+
+            beforeAll(() => {
+              result = containerApi.get(typeFixture);
+            });
+
+            it('should return an instance', () => {
+              const expected: unknown = new typeFixture(
+                new parameterTypeFixture(),
+              );
+
+              (expected as Record<string, unknown>)['property'] =
+                new propertyTypeFixture();
+
+              expect(result).toStrictEqual(expected);
+            });
+          });
+        });
+      });
+    });
+  });
+});
---
+++ packages/iocuak/container/modules/api/ContainerApi.ts
@@ -0,0 +1,140 @@
+import * as cuaktask from '@cuaklabs/cuaktask';
+
+import { Newable } from '../../../task/models/domain/Newable';
+import { ServiceId } from '../../../task/models/domain/ServiceId';
+import { TaskKind } from '../../../task/models/domain/TaskKind';
+import { TaskBuilder } from '../../../task/modules/TaskBuilder';
+import { TaskDependencyEngine } from '../../../task/modules/TaskDependencyEngine';
+import { TaskKindSet } from '../../../task/modules/TaskKindSet';
+import { ContainerApiService } from '../../services/api/ContainerApiService';
+import { ContainerApiServiceImplementation } from '../../services/api/ContainerApiServiceImplementation';
+import { ContainerInstanceServiceImplementation } from '../../services/cuaktask/ContainerInstanceServiceImplementation';
+import { ContainerBindingService } from '../../services/domain/ContainerBindingService';
+import { ContainerBindingServiceImplementation } from '../../services/domain/ContainerBindingServiceImplementation';
+import { ContainerInstanceService } from '../../services/domain/ContainerInstanceService';
+import { ContainerMetadataService } from '../../services/domain/ContainerMetadataService';
+import { ContainerMetadataServiceImplementation } from '../../services/domain/ContainerMetadataServiceImplementation';
+import { ContainerService } from '../../services/domain/ContainerService';
+import { ContainerSingletonService } from '../../services/domain/ContainerSingletonService';
+import { ContainerSingletonServiceImplementation } from '../../services/domain/ContainerSingletonServiceImplementation';
+import { ContainerModuleApi } from './ContainerModuleApi';
+
+export class ContainerApi implements ContainerApiService {
+  // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
+  readonly #containerApiService: ContainerApiService;
+
+  constructor() {
+    const containerApiService: ContainerApiService =
+      this.#initializeContainerApiService();
+
+    this.#containerApiService = containerApiService;
+  }
+
+  public bind<TInstance, TArgs extends unknown[]>(
+    type: Newable<TInstance, TArgs>,
+  ): void {
+    this.#containerApiService.bind(type);
+  }
+
+  public get<TInstance>(serviceId: ServiceId): TInstance {
+    return this.#containerApiService.get(serviceId);
+  }
+
+  public load(containerModuleApi: ContainerModuleApi): void {
+    this.#containerApiService.load(containerModuleApi);
+  }
+
+  public unbind(serviceId: ServiceId): void {
+    this.#containerApiService.unbind(serviceId);
+  }
+
+  #initializeContainerApiService(): ContainerApiService {
+    const containerService: ContainerService =
+      this.#initializeContainerService();
+
+    const containerApiService: ContainerApiService =
+      new ContainerApiServiceImplementation(containerService);
+
+    return containerApiService;
+  }
+
+  #initializeContainerService(): ContainerService {
+    const containerBindingService: ContainerBindingService =
+      new ContainerBindingServiceImplementation();
+    const containerMetadataService: ContainerMetadataService =
+      new ContainerMetadataServiceImplementation();
+    const containerSingletonService: ContainerSingletonService =
+      new ContainerSingletonServiceImplementation();
+
+    const containerInstanceService: ContainerInstanceService =
+      this.#initializeContainerInstanceService(
+        containerBindingService,
+        containerMetadataService,
+        containerSingletonService,
+      );
+
+    const containerService: ContainerService = {
+      binding: containerBindingService,
+      instance: containerInstanceService,
+      metadata: containerMetadataService,
+      singleton: containerSingletonService,
+    };
+    return containerService;
+  }
+
+  #initializeContainerInstanceService(
+    containerBindingService: ContainerBindingService,
+    containerMetadataService: ContainerMetadataService,
+    containerSingletonService: ContainerSingletonService,
+  ): ContainerInstanceService {
+    const dependentTaskRunner: cuaktask.DependentTaskRunner =
+      new cuaktask.DependentTaskRunner();
+
+    const taskBuilder: cuaktask.Builder<
+      [TaskKind],
+      cuaktask.DependentTask<TaskKind, TaskKind>
+    > = this.initializeTaskRunner(
+      containerBindingService,
+      containerMetadataService,
+      containerSingletonService,
+    );
+
+    const containerInstanceService: ContainerInstanceService =
+      new ContainerInstanceServiceImplementation(
+        dependentTaskRunner,
+        taskBuilder,
+      );
+    return containerInstanceService;
+  }
+
+  private initializeTaskRunner(
+    containerBindingService: ContainerBindingService,
+    containerMetadataService: ContainerMetadataService,
+    containerSingletonService: ContainerSingletonService,
+  ) {
+    const taskDependenciesKindSetBuilder: cuaktask.Builder<
+      [],
+      cuaktask.SetLike<TaskKind>
+    > = {
+      build: () => new TaskKindSet(),
+    };
+
+    const taskDependencyEngine: cuaktask.TaskDependencyEngine =
+      new TaskDependencyEngine(
+        containerBindingService,
+        containerMetadataService,
+      );
+
+    const taskBuilder: cuaktask.Builder<
+      [TaskKind],
+      cuaktask.DependentTask<TaskKind, TaskKind>
+    > = new TaskBuilder(
+      taskDependenciesKindSetBuilder,
+      taskDependencyEngine,
+      containerBindingService,
+      containerSingletonService,
+    );
+
+    return taskBuilder;
+  }
+}
