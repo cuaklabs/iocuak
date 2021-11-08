4b7834128f7b34a6418e0e8729bb8b8f8478ec81
notaphplover <roberto.pintos.lopez@gmail.com>
2021-11-08 19:17:05 +0100

    🏷️ : update TypeBinding.bindingType to be BindingType.type

--- packages/iocuak/binding/models/domain/TypeBinding.ts
+++ packages/iocuak/binding/models/domain/TypeBinding.ts
@@ -1,11 +1,13 @@
 import { Newable } from '../../../common/models/domain/Newable';
 import { TaskScope } from '../../../task/models/domain/TaskScope';
 import { BaseBinding } from './BaseBinding';
+import { BindingType } from './BindingType';
 
 export interface TypeBinding<
   TInstance = unknown,
   TArgs extends unknown[] = unknown[],
 > extends BaseBinding {
+  bindingType: BindingType.type;
   scope: TaskScope;
   type: Newable<TInstance, TArgs>;
 }
