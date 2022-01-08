4d0d4e8e52cdc3f0a8d469a0550b7d58c901adb6
notaphplover <roberto.pintos.lopez@gmail.com>
2022-01-08 23:27:17 +0100

    refactor(iocuak): update ContainerApi to inherit ContainerApiService

--- packages/iocuak/container/modules/api/ContainerApi.ts
+++ packages/iocuak/container/modules/api/ContainerApi.ts
@@ -1,12 +1,9 @@
 import * as cuaktask from '@cuaklabs/cuaktask';
 
-import { Newable } from '../../../common/models/domain/Newable';
-import { ServiceId } from '../../../common/models/domain/ServiceId';
 import { TaskKind } from '../../../task/models/domain/TaskKind';
 import { TaskBuilder } from '../../../task/modules/TaskBuilder';
 import { TaskDependencyEngine } from '../../../task/modules/TaskDependencyEngine';
 import { TaskKindSet } from '../../../task/modules/TaskKindSet';
-import { ContainerApiService } from '../../services/api/ContainerApiService';
 import { ContainerApiServiceImplementation } from '../../services/api/ContainerApiServiceImplementation';
 import { ContainerInstanceServiceImplementation } from '../../services/cuaktask/ContainerInstanceServiceImplementation';
 import { ContainerBindingService } from '../../services/domain/ContainerBindingService';
@@ -19,20 +16,10 @@ import { ContainerRequestServiceImplementation } from '../../services/domain/Con
 import { ContainerService } from '../../services/domain/ContainerService';
 import { ContainerSingletonService } from '../../services/domain/ContainerSingletonService';
 import { ContainerSingletonServiceImplementation } from '../../services/domain/ContainerSingletonServiceImplementation';
-import { ContainerModuleApi } from './ContainerModuleApi';
 
-export class ContainerApi implements ContainerApiService {
-  readonly #containerService: ContainerService;
-  readonly #containerApiService: ContainerApiService;
-
+export class ContainerApi extends ContainerApiServiceImplementation {
   private constructor(containerService?: ContainerService) {
-    const instanceContainerService: ContainerService =
-      containerService ?? this.#initializeContainerService();
-    const containerApiService: ContainerApiService =
-      this.#initializeContainerApiService(instanceContainerService);
-
-    this.#containerService = instanceContainerService;
-    this.#containerApiService = containerApiService;
+    super(containerService ?? ContainerApi.#initializeContainerService());
   }
 
   public static build(): ContainerApi {
@@ -41,48 +28,7 @@ export class ContainerApi implements ContainerApiService {
     return containerApi;
   }
 
-  public bind<TInstance, TArgs extends unknown[]>(
-    type: Newable<TInstance, TArgs>,
-  ): void {
-    this.#containerApiService.bind(type);
-  }
-
-  public bindToValue<TInstance>(serviceId: ServiceId, value: TInstance): void {
-    this.#containerApiService.bindToValue(serviceId, value);
-  }
-
-  public createChild(): ContainerApi {
-    const childContainerService: ContainerService =
-      this.#initializeContainerService(this.#containerService.binding);
-    const childContainerApi: ContainerApi = new ContainerApi(
-      childContainerService,
-    );
-
-    return childContainerApi;
-  }
-
-  public get<TInstance>(serviceId: ServiceId): TInstance {
-    return this.#containerApiService.get(serviceId);
-  }
-
-  public load(containerModuleApi: ContainerModuleApi): void {
-    this.#containerApiService.load(containerModuleApi);
-  }
-
-  public unbind(serviceId: ServiceId): void {
-    this.#containerApiService.unbind(serviceId);
-  }
-
-  #initializeContainerApiService(
-    containerService: ContainerService,
-  ): ContainerApiService {
-    const containerApiService: ContainerApiService =
-      new ContainerApiServiceImplementation(containerService);
-
-    return containerApiService;
-  }
-
-  #initializeContainerService(
+  static #initializeContainerService(
     parentContainerBindingService?: ContainerBindingService,
   ): ContainerService {
     const containerBindingService: ContainerBindingService =
@@ -112,7 +58,7 @@ export class ContainerApi implements ContainerApiService {
     return containerService;
   }
 
-  #initializeContainerInstanceService(
+  static #initializeContainerInstanceService(
     containerBindingService: ContainerBindingService,
     containerMetadataService: ContainerMetadataService,
     containerRequestService: ContainerRequestService,
@@ -140,7 +86,7 @@ export class ContainerApi implements ContainerApiService {
     return containerInstanceService;
   }
 
-  #initializeTaskRunner(
+  static #initializeTaskRunner(
     containerBindingService: ContainerBindingService,
     containerMetadataService: ContainerMetadataService,
     containerRequestService: ContainerRequestService,
@@ -170,5 +116,15 @@ export class ContainerApi implements ContainerApiService {
     );
 
     return taskBuilder;
+  }
+
+  public createChild(): ContainerApi {
+    const childContainerService: ContainerService =
+      ContainerApi.#initializeContainerService(this._containerService.binding);
+    const childContainerApi: ContainerApi = new ContainerApi(
+      childContainerService,
+    );
+
+    return childContainerApi;
   }
 }
