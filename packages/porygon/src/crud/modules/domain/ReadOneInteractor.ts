b537698006e1f54e3ad5f85b4935ec2d9ab37835
notaphplover <roberto.pintos.lopez@gmail.com>
2022-03-11 15:44:03 +0100

    refactor(porygon): update ReadOneInteractor with ecmascript private properties

--- packages/porygon/src/crud/modules/domain/ReadOneInteractor.ts
+++ packages/porygon/src/crud/modules/domain/ReadOneInteractor.ts
@@ -4,12 +4,15 @@ import { Interactor } from './Interactor';
 export class ReadOneInteractor<TModel, TQuery>
   implements Interactor<TQuery, TModel | undefined>
 {
-  constructor(private readonly findAdapter: FindAdapter<TModel, TQuery>) {}
+  readonly #findAdapter: FindAdapter<TModel, TQuery>;
+
+  constructor(findAdapter: FindAdapter<TModel, TQuery>) {
+    this.#findAdapter = findAdapter;
+  }
 
   public async interact(query: TQuery): Promise<TModel | undefined> {
-    const modelOrUndefined: TModel | undefined = await this.findAdapter.findOne(
-      query,
-    );
+    const modelOrUndefined: TModel | undefined =
+      await this.#findAdapter.findOne(query);
 
     return modelOrUndefined;
   }
