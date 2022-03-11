26edb681a1a33c55ef7c4af0c7984c00639ddb71
notaphplover <roberto.pintos.lopez@gmail.com>
2022-03-11 15:44:19 +0100

    refactor(porygon): update ReadManyInteractor with ecmascript private properties

--- packages/porygon/src/crud/modules/domain/ReadManyInteractor.ts
+++ packages/porygon/src/crud/modules/domain/ReadManyInteractor.ts
@@ -4,10 +4,14 @@ import { Interactor } from './Interactor';
 export class ReadManyInteractor<TModel, TQuery>
   implements Interactor<TQuery, TModel[]>
 {
-  constructor(private readonly findAdapter: FindAdapter<TModel, TQuery>) {}
+  readonly #findAdapter: FindAdapter<TModel, TQuery>;
+
+  constructor(findAdapter: FindAdapter<TModel, TQuery>) {
+    this.#findAdapter = findAdapter;
+  }
 
   public async interact(query: TQuery): Promise<TModel[]> {
-    const models: TModel[] = await this.findAdapter.find(query);
+    const models: TModel[] = await this.#findAdapter.find(query);
 
     return models;
   }
