86babd4b7c9e10832d2f62808e0db1066f4e25fd
notaphplover <roberto.pintos.lopez@gmail.com>
2022-03-11 21:36:37 +0100

    refactor(porygon): add CreationContainerModuleApi

---
+++ packages/porygon/src/crud/modules/iocuak/CreationContainerModuleApi.spec.ts
@@ -0,0 +1,127 @@
+jest.mock('@cuaklabs/iocuak');
+
+import { ContainerApi, inject, injectable, TaskScope } from '@cuaklabs/iocuak';
+
+import { CrudModuleType } from '../../models/domain/CrudModuleType';
+import { ModuleTypeToSymbolMap } from '../../models/domain/ModuleTypeToSymbolMap';
+import { CreateEntityInteractor } from '../domain/CreateEntityInteractor';
+import { CreationContainerModuleApi } from './CreationContainerModuleApi';
+
+interface ModelTest {
+  foo: string;
+}
+
+interface QueryTest {
+  bar: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+function expectClassExtending(superclass: Function): Function {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return expect.objectContaining<Partial<Function>>({
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    prototype: expect.any(superclass) as Function,
+    // eslint-disable-next-line @typescript-eslint/ban-types
+  }) as Function;
+}
+
+describe(CreationContainerModuleApi.name, () => {
+  let crudModuleTypeToSymbolMap: ModuleTypeToSymbolMap<CrudModuleType>;
+  let creationContainerModuleApi: CreationContainerModuleApi<
+    ModelTest,
+    QueryTest
+  >;
+
+  beforeAll(() => {
+    crudModuleTypeToSymbolMap = Object.freeze({
+      [CrudModuleType.createEntityAdapter]: Symbol(),
+      [CrudModuleType.createEntityInteractor]: Symbol(),
+      [CrudModuleType.deleteEntityAdapter]: Symbol(),
+      [CrudModuleType.deleteEntityInteractor]: Symbol(),
+      [CrudModuleType.readEntityAdapter]: Symbol(),
+      [CrudModuleType.readManyEntityInteractor]: Symbol(),
+      [CrudModuleType.readOneEntityInteractor]: Symbol(),
+      [CrudModuleType.updateEntityAdapter]: Symbol(),
+      [CrudModuleType.updateEntityInteractor]: Symbol(),
+    });
+
+    creationContainerModuleApi = new CreationContainerModuleApi(
+      crudModuleTypeToSymbolMap,
+    );
+  });
+
+  describe('.load()', () => {
+    let containerApiMock: jest.Mocked<ContainerApi>;
+
+    beforeAll(() => {
+      containerApiMock = {
+        bind: jest.fn(),
+      } as Partial<jest.Mocked<ContainerApi>> as jest.Mocked<ContainerApi>;
+    });
+
+    describe('when called', () => {
+      let injectDecoratorMock: jest.Mock<
+        void,
+        // eslint-disable-next-line @typescript-eslint/ban-types
+        [Object, string | symbol, number | void]
+      >;
+      // eslint-disable-next-line @typescript-eslint/ban-types
+      let injectableDecoratorMock: jest.Mock<void, [Function]>;
+
+      beforeAll(() => {
+        injectDecoratorMock = jest.fn<
+          void,
+          // eslint-disable-next-line @typescript-eslint/ban-types
+          [Object, string | symbol, number | void]
+        >();
+
+        (
+          inject as jest.Mock<PropertyDecorator & ParameterDecorator>
+        ).mockReturnValueOnce(injectDecoratorMock);
+
+        // eslint-disable-next-line @typescript-eslint/ban-types
+        injectableDecoratorMock = jest.fn<void, [Function]>();
+
+        (injectable as jest.Mock<ClassDecorator>).mockReturnValueOnce(
+          injectableDecoratorMock,
+        );
+
+        creationContainerModuleApi.load(containerApiMock);
+      });
+
+      it('should call injectable()', () => {
+        expect(injectable).toHaveBeenCalledTimes(1);
+        expect(injectableDecoratorMock).toHaveBeenCalledTimes(1);
+
+        expect(injectable).toHaveBeenCalledWith({
+          id: crudModuleTypeToSymbolMap.createEntityInteractor,
+          scope: TaskScope.singleton,
+        });
+
+        expect(injectableDecoratorMock).toHaveBeenCalledWith(
+          expectClassExtending(CreateEntityInteractor),
+        );
+      });
+
+      it('should call inject()', () => {
+        expect(inject).toHaveBeenCalledTimes(1);
+        expect(injectDecoratorMock).toHaveBeenCalledTimes(1);
+
+        expect(inject).toHaveBeenCalledWith(CrudModuleType.createEntityAdapter);
+
+        expect(injectDecoratorMock).toHaveBeenCalledWith(
+          expectClassExtending(CreateEntityInteractor),
+          undefined,
+          0,
+        );
+      });
+
+      it('should call containerApi.bind()', () => {
+        expect(containerApiMock.bind).toHaveBeenCalledTimes(1);
+        expect(containerApiMock.bind).toHaveBeenCalledWith(
+          expectClassExtending(CreateEntityInteractor),
+        );
+      });
+    });
+  });
+});
---
+++ packages/porygon/src/crud/modules/iocuak/CreationContainerModuleApi.ts
@@ -0,0 +1,58 @@
+import {
+  ContainerApiService,
+  ContainerModuleApi,
+  inject,
+  injectable,
+  Newable,
+  ServiceId,
+  TaskScope,
+} from '@cuaklabs/iocuak';
+
+import { CreateAdapter } from '../../adapter/domain/CreateAdapter';
+import { CrudModuleType } from '../../models/domain/CrudModuleType';
+import { ModuleTypeToSymbolMap } from '../../models/domain/ModuleTypeToSymbolMap';
+import { CreateEntityInteractor } from '../domain/CreateEntityInteractor';
+import { Interactor } from '../domain/Interactor';
+
+export class CreationContainerModuleApi<TModel, TQuery>
+  implements ContainerModuleApi
+{
+  readonly #crudModuleTypeToSymbolMap: ModuleTypeToSymbolMap<CrudModuleType>;
+  readonly #createEntityInteractorType: Newable<
+    Interactor<TQuery, TModel>,
+    [CreateAdapter<TModel, TQuery>]
+  >;
+
+  constructor(
+    crudModuleTypeToSymbolMap: ModuleTypeToSymbolMap<CrudModuleType>,
+  ) {
+    this.#crudModuleTypeToSymbolMap = crudModuleTypeToSymbolMap;
+
+    this.#createEntityInteractorType = class extends CreateEntityInteractor<
+      TModel,
+      TQuery
+    > {};
+  }
+
+  public load(container: ContainerApiService): void {
+    this.#loadCreateEntityInteractor(container);
+  }
+
+  #loadCreateEntityInteractor(container: ContainerApiService): void {
+    const createEntityInteractorServiceId: ServiceId =
+      this.#crudModuleTypeToSymbolMap[CrudModuleType.createEntityInteractor];
+
+    injectable({
+      id: createEntityInteractorServiceId,
+      scope: TaskScope.singleton,
+    })(this.#createEntityInteractorType);
+
+    inject(CrudModuleType.createEntityAdapter)(
+      this.#createEntityInteractorType,
+      undefined as unknown as string | symbol,
+      0,
+    );
+
+    container.bind(this.#createEntityInteractorType);
+  }
+}
