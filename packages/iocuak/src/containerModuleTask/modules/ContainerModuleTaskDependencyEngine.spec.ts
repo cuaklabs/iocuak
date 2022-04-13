7f46cf765dd869f62b9891c966d1dd6e32032932
notaphplover <roberto.pintos.lopez@gmail.com>
2022-04-13 16:31:00 +0200

    refactor(iocuak): add ContainerModuleTaskDependencyEngine

---
+++ packages/iocuak/src/containerModuleTask/modules/ContainerModuleTaskDependencyEngine.spec.ts
@@ -0,0 +1,281 @@
+import * as cuaktask from '@cuaklabs/cuaktask';
+
+import { ContainerModuleCreateInstancesTaskKindMocks } from '../mocks/models/domain/ContainerModuleCreateInstancesTaskKindMocks';
+import { ContainerModuleLoadFromMetadataTaskKindMocks } from '../mocks/models/domain/ContainerModuleLoadFromMetadataTaskKindMocks';
+import { ContainerModuleMetadataMocks } from '../mocks/models/domain/ContainerModuleMetadataMocks';
+import { ContainerModuleCreateInstancesTaskKind } from '../models/domain/ContainerModuleCreateInstancesTaskKind';
+import { ContainerModuleLoadFromMetadataTaskKind } from '../models/domain/ContainerModuleLoadFromMetadataTaskKind';
+import { ContainerModuleMetadata } from '../models/domain/ContainerModuleMetadata';
+import { ContainerModuleTaskKind } from '../models/domain/ContainerModuleTaskKind';
+import { ContainerModuleTaskKindType } from '../models/domain/ContainerModuleTaskKindType';
+import { ContainerModuleTaskDependencyEngine } from './ContainerModuleTaskDependencyEngine';
+
+describe(ContainerModuleTaskDependencyEngine.name, () => {
+  let containerModuleTaskDependencyEngine: ContainerModuleTaskDependencyEngine;
+
+  beforeAll(() => {
+    containerModuleTaskDependencyEngine =
+      new ContainerModuleTaskDependencyEngine();
+  });
+
+  describe('.getDependencies', () => {
+    describe('having a task kind of type createInstances', () => {
+      let taskKindFixture: ContainerModuleCreateInstancesTaskKind;
+
+      beforeAll(() => {
+        taskKindFixture = ContainerModuleCreateInstancesTaskKindMocks.any;
+      });
+
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(() => {
+          try {
+            containerModuleTaskDependencyEngine.getDependencies(
+              taskKindFixture,
+            );
+          } catch (error: unknown) {
+            result = error;
+          }
+        });
+
+        it('should throw an error', () => {
+          expect(result).toBeInstanceOf(Error);
+          expect(result).toStrictEqual(
+            expect.objectContaining<Partial<Error>>({
+              message: 'Unsupported type',
+            }),
+          );
+        });
+      });
+    });
+
+    describe('having a task kind with metadata with no imports nor injects', () => {
+      let taskKindFixture: ContainerModuleLoadFromMetadataTaskKind;
+
+      beforeAll(() => {
+        taskKindFixture =
+          ContainerModuleLoadFromMetadataTaskKindMocks.withMetadataWithImportsEmptyAndInjectsEmpty;
+      });
+
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(() => {
+          result =
+            containerModuleTaskDependencyEngine.getDependencies(
+              taskKindFixture,
+            );
+        });
+
+        it('should return a task kind graph', () => {
+          const expectedTaskKindGraphNode: cuaktask.TaskDependencyKindGraphNode<
+            ContainerModuleTaskKind,
+            ContainerModuleTaskKind
+          > = {
+            dependencies: [],
+            kind: taskKindFixture,
+          };
+
+          const expectedTaskKindGraph: cuaktask.TaskDependencyKindGraph<
+            ContainerModuleTaskKind,
+            ContainerModuleTaskKind
+          > = {
+            nodes: [expectedTaskKindGraphNode],
+            rootNode: expectedTaskKindGraphNode,
+          };
+
+          expect(result).toStrictEqual(expectedTaskKindGraph);
+        });
+      });
+    });
+
+    describe('having a task with metadata with no imports and injects', () => {
+      let taskKindFixture: ContainerModuleLoadFromMetadataTaskKind;
+
+      beforeAll(() => {
+        taskKindFixture =
+          ContainerModuleLoadFromMetadataTaskKindMocks.withMetadataWithImportsEmptyAndInjectsWithOneServiceId;
+      });
+
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(() => {
+          result =
+            containerModuleTaskDependencyEngine.getDependencies(
+              taskKindFixture,
+            );
+        });
+
+        it('should return a task kind graph', () => {
+          const expectedCreateInstancesTaskNode: cuaktask.TaskDependencyKindGraphNode<
+            ContainerModuleTaskKind,
+            ContainerModuleTaskKind
+          > = {
+            dependencies: [],
+            kind: {
+              serviceIds: taskKindFixture.metadata.injects,
+              type: ContainerModuleTaskKindType.createInstances,
+            },
+          };
+          const expectedTaskKindGraphNode: cuaktask.TaskDependencyKindGraphNode<
+            ContainerModuleTaskKind,
+            ContainerModuleTaskKind
+          > = {
+            dependencies: [expectedCreateInstancesTaskNode],
+            kind: taskKindFixture,
+          };
+
+          const expectedTaskKindGraph: cuaktask.TaskDependencyKindGraph<
+            ContainerModuleTaskKind,
+            ContainerModuleTaskKind
+          > = {
+            nodes: [expectedTaskKindGraphNode, expectedCreateInstancesTaskNode],
+            rootNode: expectedTaskKindGraphNode,
+          };
+
+          expect(result).toStrictEqual(expectedTaskKindGraph);
+        });
+      });
+    });
+
+    describe('having a task with imports and no injects', () => {
+      let containerModuleMetadataFixture: ContainerModuleMetadata;
+      let dependencyContainerModuleMetadataFixture: ContainerModuleMetadata;
+      let taskKindFixture: ContainerModuleLoadFromMetadataTaskKind;
+
+      beforeAll(() => {
+        dependencyContainerModuleMetadataFixture =
+          ContainerModuleMetadataMocks.withImportsEmptyAndInjectsEmpty;
+
+        containerModuleMetadataFixture = {
+          ...ContainerModuleMetadataMocks.withImportsEmptyAndInjectsEmpty,
+          imports: [dependencyContainerModuleMetadataFixture],
+        };
+
+        taskKindFixture = {
+          ...ContainerModuleLoadFromMetadataTaskKindMocks.any,
+          metadata: containerModuleMetadataFixture,
+        };
+      });
+
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(() => {
+          result =
+            containerModuleTaskDependencyEngine.getDependencies(
+              taskKindFixture,
+            );
+        });
+
+        it('should return a task kind graph', () => {
+          const expectedDependencyTaskNode: cuaktask.TaskDependencyKindGraphNode<
+            ContainerModuleTaskKind,
+            ContainerModuleTaskKind
+          > = {
+            dependencies: [],
+            kind: {
+              metadata: dependencyContainerModuleMetadataFixture,
+              type: ContainerModuleTaskKindType.loadFromMetadata,
+            },
+          };
+          const expectedTaskKindGraphNode: cuaktask.TaskDependencyKindGraphNode<
+            ContainerModuleTaskKind,
+            ContainerModuleTaskKind
+          > = {
+            dependencies: [expectedDependencyTaskNode],
+            kind: taskKindFixture,
+          };
+
+          const expectedTaskKindGraph: cuaktask.TaskDependencyKindGraph<
+            ContainerModuleTaskKind,
+            ContainerModuleTaskKind
+          > = {
+            nodes: [expectedTaskKindGraphNode, expectedDependencyTaskNode],
+            rootNode: expectedTaskKindGraphNode,
+          };
+
+          expect(result).toStrictEqual(expectedTaskKindGraph);
+        });
+      });
+    });
+
+    describe('having a task with imports and injects', () => {
+      let containerModuleMetadataFixture: ContainerModuleMetadata;
+      let dependencyContainerModuleMetadataFixture: ContainerModuleMetadata;
+      let taskKindFixture: ContainerModuleLoadFromMetadataTaskKind;
+
+      beforeAll(() => {
+        dependencyContainerModuleMetadataFixture =
+          ContainerModuleMetadataMocks.withImportsEmptyAndInjectsEmpty;
+
+        containerModuleMetadataFixture = {
+          ...ContainerModuleMetadataMocks.withImportsEmptyAndInjectsWithOneServiceId,
+          imports: [dependencyContainerModuleMetadataFixture],
+        };
+
+        taskKindFixture = {
+          ...ContainerModuleLoadFromMetadataTaskKindMocks.any,
+          metadata: containerModuleMetadataFixture,
+        };
+      });
+
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(() => {
+          result =
+            containerModuleTaskDependencyEngine.getDependencies(
+              taskKindFixture,
+            );
+        });
+
+        it('should return a task kind graph', () => {
+          const expectedDependencyTaskNode: cuaktask.TaskDependencyKindGraphNode<
+            ContainerModuleTaskKind,
+            ContainerModuleTaskKind
+          > = {
+            dependencies: [],
+            kind: {
+              metadata: dependencyContainerModuleMetadataFixture,
+              type: ContainerModuleTaskKindType.loadFromMetadata,
+            },
+          };
+          const expectedCreateInstancesTaskNode: cuaktask.TaskDependencyKindGraphNode<
+            ContainerModuleTaskKind,
+            ContainerModuleTaskKind
+          > = {
+            dependencies: [expectedDependencyTaskNode],
+            kind: {
+              serviceIds: taskKindFixture.metadata.injects,
+              type: ContainerModuleTaskKindType.createInstances,
+            },
+          };
+          const expectedTaskKindGraphNode: cuaktask.TaskDependencyKindGraphNode<
+            ContainerModuleTaskKind,
+            ContainerModuleTaskKind
+          > = {
+            dependencies: [expectedCreateInstancesTaskNode],
+            kind: taskKindFixture,
+          };
+
+          const expectedTaskKindGraph: cuaktask.TaskDependencyKindGraph<
+            ContainerModuleTaskKind,
+            ContainerModuleTaskKind
+          > = {
+            nodes: [
+              expectedTaskKindGraphNode,
+              expectedCreateInstancesTaskNode,
+              expectedDependencyTaskNode,
+            ],
+            rootNode: expectedTaskKindGraphNode,
+          };
+
+          expect(result).toStrictEqual(expectedTaskKindGraph);
+        });
+      });
+    });
+  });
+});
---
+++ packages/iocuak/src/containerModuleTask/modules/ContainerModuleTaskDependencyEngine.ts
@@ -0,0 +1,130 @@
+import * as cuaktask from '@cuaklabs/cuaktask';
+
+import { ServiceId } from '../../common/models/domain/ServiceId';
+import { ContainerModuleLoadFromMetadataTaskKind } from '../models/domain/ContainerModuleLoadFromMetadataTaskKind';
+import { ContainerModuleMetadata } from '../models/domain/ContainerModuleMetadata';
+import { ContainerModuleTaskKind } from '../models/domain/ContainerModuleTaskKind';
+import { ContainerModuleTaskKindType } from '../models/domain/ContainerModuleTaskKindType';
+
+type LoadModuleTaskKindGraphNode = cuaktask.TaskDependencyKindGraphNode<
+  ContainerModuleLoadFromMetadataTaskKind,
+  ContainerModuleTaskKind
+>;
+
+type TaskKindGraphNode = cuaktask.TaskDependencyKindGraphNode<
+  ContainerModuleTaskKind,
+  ContainerModuleTaskKind
+>;
+
+type TaskDependencyKindGraph = cuaktask.TaskDependencyKindGraph<
+  ContainerModuleTaskKind,
+  ContainerModuleTaskKind
+>;
+
+export class ContainerModuleTaskDependencyEngine
+  implements
+    cuaktask.TaskDependencyEngine<
+      ContainerModuleTaskKind,
+      ContainerModuleTaskKind
+    >
+{
+  public getDependencies(
+    taskKind: ContainerModuleTaskKind,
+  ): TaskDependencyKindGraph {
+    switch (taskKind.type) {
+      case ContainerModuleTaskKindType.createInstances:
+        throw new Error('Unsupported type');
+      case ContainerModuleTaskKindType.loadFromMetadata:
+        return this.#getLoadFromMetadataTaskKindDependencies(taskKind);
+    }
+  }
+
+  #getLoadFromMetadataTaskKindDependencies(
+    taskKind: ContainerModuleLoadFromMetadataTaskKind,
+  ): TaskDependencyKindGraph {
+    const taskDependencyKindGraphRootNode: LoadModuleTaskKindGraphNode = {
+      dependencies: [],
+      kind: taskKind,
+    };
+
+    const taskDependencyKindGraphNodes: TaskKindGraphNode[] = [
+      ...this.#expandKindGraphNodes(taskDependencyKindGraphRootNode),
+    ];
+
+    const taskDependencyKindGraph: TaskDependencyKindGraph = {
+      nodes: taskDependencyKindGraphNodes,
+      rootNode: taskDependencyKindGraphRootNode,
+    };
+
+    return taskDependencyKindGraph;
+  }
+
+  #buildCreateInstancesTaskKindGraphNode(
+    injects: ServiceId[],
+    dependencies: LoadModuleTaskKindGraphNode[],
+  ): TaskKindGraphNode {
+    const createInstancesTaskKindGraphNode: TaskKindGraphNode = {
+      dependencies,
+      kind: {
+        serviceIds: injects,
+        type: ContainerModuleTaskKindType.createInstances,
+      },
+    };
+
+    return createInstancesTaskKindGraphNode;
+  }
+
+  #buildLoadDependencyModuleTaskKindGraphNodes(
+    loadModuleTaskKindGraphNode: LoadModuleTaskKindGraphNode,
+  ): LoadModuleTaskKindGraphNode[] {
+    const loadDependencyModuleTaskKindGraphNodes: LoadModuleTaskKindGraphNode[] =
+      loadModuleTaskKindGraphNode.kind.metadata.imports.map(
+        (containerModuleMetadata: ContainerModuleMetadata) => ({
+          dependencies: [],
+          kind: {
+            metadata: containerModuleMetadata,
+            type: ContainerModuleTaskKindType.loadFromMetadata,
+          },
+        }),
+      );
+
+    return loadDependencyModuleTaskKindGraphNodes;
+  }
+
+  *#expandKindGraphNodes(
+    loadModuleTaskKindGraphNode: LoadModuleTaskKindGraphNode,
+  ): Iterable<TaskKindGraphNode> {
+    const loadDependencyModuleTaskKindGraphNodes: LoadModuleTaskKindGraphNode[] =
+      this.#buildLoadDependencyModuleTaskKindGraphNodes(
+        loadModuleTaskKindGraphNode,
+      );
+
+    const loadModuleTaskKindGraphNodeInjects: ServiceId[] =
+      loadModuleTaskKindGraphNode.kind.metadata.injects;
+
+    if (loadModuleTaskKindGraphNodeInjects.length === 0) {
+      loadModuleTaskKindGraphNode.dependencies.push(
+        ...loadDependencyModuleTaskKindGraphNodes,
+      );
+
+      yield loadModuleTaskKindGraphNode;
+    } else {
+      const createInstancesTaskKindGraphNode: TaskKindGraphNode =
+        this.#buildCreateInstancesTaskKindGraphNode(
+          loadModuleTaskKindGraphNodeInjects,
+          loadDependencyModuleTaskKindGraphNodes,
+        );
+
+      loadModuleTaskKindGraphNode.dependencies.push(
+        createInstancesTaskKindGraphNode,
+      );
+
+      yield loadModuleTaskKindGraphNode;
+      yield createInstancesTaskKindGraphNode;
+    }
+
+    for (const loadModuleTaskKindGraphNode of loadDependencyModuleTaskKindGraphNodes) {
+      yield* this.#expandKindGraphNodes(loadModuleTaskKindGraphNode);
+    }
+  }
+}
