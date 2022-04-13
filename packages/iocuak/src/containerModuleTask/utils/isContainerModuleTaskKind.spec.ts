a6498119518045e3f4e88a1843a3d2313c00ff60
notaphplover <roberto.pintos.lopez@gmail.com>
2022-04-13 16:35:59 +0200

    refactor(iocuak): add isContainerModuleTaskKind

---
+++ packages/iocuak/src/containerModuleTask/utils/isContainerModuleTaskKind.spec.ts
@@ -0,0 +1,68 @@
+import { ContainerModuleTaskKindType } from '../models/domain/ContainerModuleTaskKindType';
+import { isContainerModuleTaskKind } from './isContainerModuleTaskKind';
+
+describe(isContainerModuleTaskKind.name, () => {
+  describe('having a value with no type', () => {
+    let valueFixture: unknown;
+
+    beforeAll(() => {
+      valueFixture = {};
+    });
+
+    describe('when called', () => {
+      let result: unknown;
+
+      beforeAll(() => {
+        result = isContainerModuleTaskKind(valueFixture);
+      });
+
+      it('should return false', () => {
+        expect(result).toBe(false);
+      });
+    });
+  });
+
+  describe('having a value with type in TaskKindType', () => {
+    let valueFixture: unknown;
+
+    beforeAll(() => {
+      valueFixture = {
+        type: ContainerModuleTaskKindType.createInstances,
+      };
+    });
+
+    describe('when called', () => {
+      let result: unknown;
+
+      beforeAll(() => {
+        result = isContainerModuleTaskKind(valueFixture);
+      });
+
+      it('should return true', () => {
+        expect(result).toBe(true);
+      });
+    });
+  });
+
+  describe('having a value with type not in TaskKindType', () => {
+    let valueFixture: unknown;
+
+    beforeAll(() => {
+      valueFixture = {
+        type: {},
+      };
+    });
+
+    describe('when called', () => {
+      let result: unknown;
+
+      beforeAll(() => {
+        result = isContainerModuleTaskKind(valueFixture);
+      });
+
+      it('should return false', () => {
+        expect(result).toBe(false);
+      });
+    });
+  });
+});
