e099391371f0165c1ade12d7b2c6e8537d8319a3
notaphplover <roberto.pintos.lopez@gmail.com>
2022-04-16 20:33:24 +0200

    refactor(iocuak): add convertToContainerModule util function

---
+++ packages/iocuak/src/containerModuleTask/utils/convertToContainerModule.spec.ts
@@ -0,0 +1,197 @@
+jest.mock('../../container/utils/bind');
+jest.mock('../../container/utils/bindToValue');
+
+import { Newable } from '../../common/models/domain/Newable';
+import { ServiceId } from '../../common/models/domain/ServiceId';
+import { ContainerModuleApi } from '../../container/modules/api/ContainerModuleApi';
+import { ContainerModule } from '../../container/modules/domain/ContainerModule';
+import { ContainerModuleBindingServiceApi } from '../../container/services/api/ContainerModuleBindingServiceApi';
+import { ContainerBindingService } from '../../container/services/domain/ContainerBindingService';
+import { bind } from '../../container/utils/bind';
+import { bindToValue } from '../../container/utils/bindToValue';
+import { MetadataService } from '../../metadata/services/domain/MetadataService';
+import { convertToContainerModule } from './convertToContainerModule';
+
+describe(convertToContainerModule.name, () => {
+  let containerModuleApiMock: jest.Mocked<ContainerModuleApi>;
+
+  beforeAll(() => {
+    containerModuleApiMock = {
+      load: jest.fn(),
+    };
+  });
+
+  describe('when called', () => {
+    let result: unknown;
+
+    beforeAll(() => {
+      result = convertToContainerModule(containerModuleApiMock);
+    });
+
+    afterAll(() => {
+      jest.clearAllMocks();
+    });
+
+    it('should return a ContainerModule', () => {
+      expect((result as ContainerModule).load).toBeInstanceOf(Function);
+    });
+
+    describe('when result.load() is called', () => {
+      let containerBindingServiceFixture: ContainerBindingService;
+      let metadataServiceFixture: MetadataService;
+
+      beforeAll(() => {
+        containerBindingServiceFixture = {
+          _tag: 'containerBindingService',
+        } as unknown as ContainerBindingService;
+
+        metadataServiceFixture = {
+          _tag: 'metadataServiceFixture',
+        } as unknown as MetadataService;
+
+        (result as ContainerModule).load(
+          containerBindingServiceFixture,
+          metadataServiceFixture,
+        );
+      });
+
+      afterAll(() => {
+        jest.clearAllMocks();
+      });
+
+      it('should call containerModuleApi.load()', () => {
+        const expected: ContainerModuleBindingServiceApi = {
+          bind: expect.any(
+            Function,
+          ) as ContainerModuleBindingServiceApi['bind'],
+          bindToValue: expect.any(
+            Function,
+          ) as ContainerModuleBindingServiceApi['bindToValue'],
+        };
+
+        expect(containerModuleApiMock.load).toHaveBeenCalledTimes(1);
+        expect(containerModuleApiMock.load).toHaveBeenCalledWith(expected);
+      });
+    });
+
+    describe('when result.load() is called and containerModuleApi.load() calls containerModuleBindingServiceApi.bind()', () => {
+      let typeFixture: Newable;
+      let containerBindingServiceFixture: ContainerBindingService;
+      let metadataServiceFixture: MetadataService;
+
+      beforeAll(() => {
+        typeFixture = class {};
+        containerModuleApiMock.load.mockImplementationOnce(
+          (
+            containerModuleBindingService: ContainerModuleBindingServiceApi,
+          ): void => {
+            containerModuleBindingService.bind(typeFixture);
+          },
+        );
+
+        containerBindingServiceFixture = {
+          _tag: 'containerBindingService',
+        } as Partial<ContainerBindingService> as ContainerBindingService;
+
+        metadataServiceFixture = {
+          _tag: 'metadataServiceFixture',
+        } as unknown as MetadataService;
+
+        (result as ContainerModule).load(
+          containerBindingServiceFixture,
+          metadataServiceFixture,
+        );
+      });
+
+      afterAll(() => {
+        jest.clearAllMocks();
+      });
+
+      it('should call containerModuleApi.load()', () => {
+        const expected: ContainerModuleBindingServiceApi = {
+          bind: expect.any(
+            Function,
+          ) as ContainerModuleBindingServiceApi['bind'],
+          bindToValue: expect.any(
+            Function,
+          ) as ContainerModuleBindingServiceApi['bindToValue'],
+        };
+
+        expect(containerModuleApiMock.load).toHaveBeenCalledTimes(1);
+        expect(containerModuleApiMock.load).toHaveBeenCalledWith(expected);
+      });
+
+      it('should call bind', () => {
+        expect(bind).toHaveBeenCalledTimes(1);
+        expect(bind).toHaveBeenCalledWith(
+          typeFixture,
+          containerBindingServiceFixture,
+          metadataServiceFixture,
+        );
+      });
+    });
+
+    describe('when result.load() is called and containerModuleApi.load() calls containerModuleBindingServiceApi.bindToValue()', () => {
+      let serviceIdFixture: ServiceId;
+      let valueFixture: unknown;
+      let containerBindingServiceFixture: ContainerBindingService;
+      let metadataServiceFixture: MetadataService;
+
+      beforeAll(() => {
+        serviceIdFixture = Symbol();
+        valueFixture = Symbol();
+
+        containerModuleApiMock.load.mockImplementationOnce(
+          (
+            containerModuleBindingService: ContainerModuleBindingServiceApi,
+          ): void => {
+            containerModuleBindingService.bindToValue(
+              serviceIdFixture,
+              valueFixture,
+            );
+          },
+        );
+
+        containerBindingServiceFixture = {
+          _tag: 'containerBindingService',
+        } as Partial<ContainerBindingService> as ContainerBindingService;
+
+        metadataServiceFixture = {
+          _tag: 'metadataServiceFixture',
+        } as unknown as MetadataService;
+
+        (result as ContainerModule).load(
+          containerBindingServiceFixture,
+          metadataServiceFixture,
+        );
+      });
+
+      afterAll(() => {
+        jest.clearAllMocks();
+      });
+
+      it('should call containerModuleApi.load()', () => {
+        const expected: ContainerModuleBindingServiceApi = {
+          bind: expect.any(
+            Function,
+          ) as ContainerModuleBindingServiceApi['bind'],
+          bindToValue: expect.any(
+            Function,
+          ) as ContainerModuleBindingServiceApi['bindToValue'],
+        };
+
+        expect(containerModuleApiMock.load).toHaveBeenCalledTimes(1);
+        expect(containerModuleApiMock.load).toHaveBeenCalledWith(expected);
+      });
+
+      it('should call bindToValue', () => {
+        expect(bindToValue).toHaveBeenCalledTimes(1);
+        expect(bindToValue).toHaveBeenCalledWith(
+          serviceIdFixture,
+          valueFixture,
+          containerBindingServiceFixture,
+        );
+      });
+    });
+  });
+});
---
+++ packages/iocuak/src/containerModuleTask/utils/convertToContainerModule.ts
@@ -0,0 +1,35 @@
+import { Newable } from '../../common/models/domain/Newable';
+import { ServiceId } from '../../common/models/domain/ServiceId';
+import { ContainerModuleApi } from '../../container/modules/api/ContainerModuleApi';
+import { ContainerModule } from '../../container/modules/domain/ContainerModule';
+import { ContainerModuleBindingServiceApi } from '../../container/services/api/ContainerModuleBindingServiceApi';
+import { ContainerBindingService } from '../../container/services/domain/ContainerBindingService';
+import { bind } from '../../container/utils/bind';
+import { bindToValue } from '../../container/utils/bindToValue';
+import { MetadataService } from '../../metadata/services/domain/MetadataService';
+
+export function convertToContainerModule(
+  containerModuleApi: ContainerModuleApi,
+): ContainerModule {
+  const containerModule: ContainerModule = {
+    load: (
+      containerBindingService: ContainerBindingService,
+      metadataService: MetadataService,
+    ): void => {
+      const containerModuleBindingServiceApi: ContainerModuleBindingServiceApi =
+        {
+          bind: <TInstance, TArgs extends unknown[]>(
+            type: Newable<TInstance, TArgs>,
+          ) => bind(type, containerBindingService, metadataService),
+          bindToValue: <TInstance>(
+            serviceId: ServiceId,
+            value: TInstance,
+          ): void => bindToValue(serviceId, value, containerBindingService),
+        };
+
+      containerModuleApi.load(containerModuleBindingServiceApi);
+    },
+  };
+
+  return containerModule;
+}
