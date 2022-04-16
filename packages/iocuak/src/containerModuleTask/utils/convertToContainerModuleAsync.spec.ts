8631642fe92fdd3423a3ede76ded48efe64d8c9f
notaphplover <roberto.pintos.lopez@gmail.com>
2022-04-16 20:37:29 +0200

    refactor(iocuak): add convertToContainerModuleAsync

---
+++ packages/iocuak/src/containerModuleTask/utils/convertToContainerModuleAsync.spec.ts
@@ -0,0 +1,28 @@
+jest.mock('./convertToContainerModule');
+
+import { ContainerModuleApi } from '../../container/modules/api/ContainerModuleApi';
+import { convertToContainerModule } from './convertToContainerModule';
+import { convertToContainerModuleAsync } from './convertToContainerModuleAsync';
+
+describe(convertToContainerModuleAsync.name, () => {
+  describe('when called', () => {
+    let containerModuleApiFixture: ContainerModuleApi;
+
+    beforeAll(async () => {
+      containerModuleApiFixture = {
+        _tag: 'containerModuleApiFixture',
+      } as unknown as ContainerModuleApi;
+
+      await convertToContainerModuleAsync(
+        Promise.resolve(containerModuleApiFixture),
+      );
+    });
+
+    it('should call convertToContainerModule()', () => {
+      expect(convertToContainerModule).toHaveBeenCalledTimes(1);
+      expect(convertToContainerModule).toHaveBeenCalledWith(
+        containerModuleApiFixture,
+      );
+    });
+  });
+});
