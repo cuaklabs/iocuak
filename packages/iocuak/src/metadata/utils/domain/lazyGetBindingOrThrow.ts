4d8ee10a43f35e4d5cde6462b34c5a815467b068
notaphplover <roberto.pintos.lopez@gmail.com>
2022-04-22 02:14:50 +0200

    refactor(iocuak): update lazyGetBindingOrThrow with TInstance and TArgs generics

--- packages/iocuak/src/metadata/utils/domain/lazyGetBindingOrThrow.ts
+++ packages/iocuak/src/metadata/utils/domain/lazyGetBindingOrThrow.ts
@@ -1,16 +1,19 @@
+import { Newable } from '../../../common/models/domain/Newable';
 import { ServiceId } from '../../../common/models/domain/ServiceId';
 import { stringifyServiceId } from '../../../utils/stringifyServiceId';
 import { Binding } from '../../models/domain/Binding';
 import { BindingType } from '../../models/domain/BindingType';
 import { getDefaultBindingScope } from './getDefaultBindingScope';
 
-export function lazyGetBindingOrThrow(serviceId: ServiceId): Binding {
+export function lazyGetBindingOrThrow<TInstance, TArgs extends unknown[]>(
+  serviceId: ServiceId,
+): Binding<TInstance, TArgs> {
   if (serviceId instanceof Function) {
     return {
       bindingType: BindingType.type,
       id: serviceId,
       scope: getDefaultBindingScope(),
-      type: serviceId,
+      type: serviceId as Newable<TInstance, TArgs>,
     };
   } else {
     throw new Error(
