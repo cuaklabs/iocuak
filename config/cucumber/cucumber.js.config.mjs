ad8b09844c36c1962ac7b5b3d6c9aa338cd69d6e
notaphplover <roberto.pintos.lopez@gmail.com>
2022-04-24 23:13:21 +0200

    chore(cucumber): add cucumber config

---
+++ config/cucumber/cucumber.js.config.mjs
@@ -0,0 +1,34 @@
+import { getBaseConfiguration } from './cucumber.config.base.mjs';
+
+/**
+ * @param {string | undefined} packageName
+ * @returns { string }
+ */
+function getRequiredModulesPaths(packageName) {
+  return `packages/${packageName ?? '*'}/lib/e2e/stepDefinitions/**/*.js`;
+}
+
+/**
+ * @param {string | undefined} packageName
+ * @param {boolean} parallel
+ * @returns {!import("@cucumber/cucumber/lib/configuration").IConfiguration}
+ */
+function getConfiguration(packageName, parallel) {
+  /** @type {!import("@cucumber/cucumber/lib/configuration").IConfiguration} */
+  const config = {
+    ...getBaseConfiguration(packageName, parallel),
+    require: [getRequiredModulesPaths(packageName)],
+  };
+
+  return config;
+}
+
+/** @type {import("@cucumber/cucumber/lib/configuration").IConfiguration} */
+const ci = getConfiguration(undefined, false);
+
+/** @type {import("@cucumber/cucumber/lib/configuration").IConfiguration} */
+const defaultCucumberConfig = getConfiguration(undefined, true);
+
+export default defaultCucumberConfig;
+
+export { ci };
