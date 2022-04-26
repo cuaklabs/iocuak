3cdb8b1f7f144a246e36d290c89942e9d3857634
notaphplover <roberto.pintos.lopez@gmail.com>
2022-04-26 20:44:16 +0200

    test(iocuak): update common definitions with result definitions

--- packages/iocuak/src/e2e/definitions/common/stepDefinitions/commonDefinitions.ts
+++ packages/iocuak/src/e2e/definitions/common/stepDefinitions/commonDefinitions.ts
@@ -1,5 +1,7 @@
-import { Given } from '@cucumber/cucumber';
+import { Given, Then } from '@cucumber/cucumber';
+import chai from 'chai';
 
+import { ResultWorld } from '../models/worlds/ResultWorld';
 import { TypeServiceWorld } from '../models/worlds/TypeServiceWorld';
 import { ValueServiceWorld } from '../models/worlds/ValueServiceWorld';
 import { TypeServiceParameter } from '../parameters/typeService/TypeServiceParameter';
@@ -18,3 +20,11 @@ Given<ValueServiceWorld>(
     this.valueServiceParameter = valueServiceParameter;
   },
 );
+
+Then<ResultWorld>('an error is thrown', function () {
+  chai.expect(this.error).not.to.be.equal(undefined);
+});
+
+Then<ResultWorld>('no errors are thrown', function () {
+  chai.expect(this.error).to.be.equal(undefined);
+});
