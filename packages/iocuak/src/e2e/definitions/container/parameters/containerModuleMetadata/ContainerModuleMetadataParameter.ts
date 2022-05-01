1b66ce097598ba4b5498b4b727f0c56e1b9b977f
notaphplover <roberto.pintos.lopez@gmail.com>
2022-05-02 01:01:47 +0200

    test(iocuak): update ContainerModuleMetadataParameter with module metadata generic

--- packages/iocuak/src/e2e/definitions/container/parameters/containerModuleMetadata/ContainerModuleMetadataParameter.ts
+++ packages/iocuak/src/e2e/definitions/container/parameters/containerModuleMetadata/ContainerModuleMetadataParameter.ts
@@ -2,8 +2,10 @@ import sinon from 'sinon';
 
 import { ContainerModuleMetadataApi } from '../../../../../containerModuleTask/models/api/ContainerModuleMetadataApi';
 
-export interface ContainerModuleMetadataParameter {
-  containerModuleMetadata: ContainerModuleMetadataApi;
+export interface ContainerModuleMetadataParameter<
+  TContainerModuleMetadata extends ContainerModuleMetadataApi = ContainerModuleMetadataApi,
+> {
+  containerModuleMetadata: TContainerModuleMetadata;
   importParameters?: ContainerModuleMetadataParameter[];
   loadSpy: sinon.SinonSpy;
   spy: sinon.SinonSpy;
