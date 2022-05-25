97d540be5381534cd3135f1d6d4cff16b1c9b453
notaphplover <roberto.pintos.lopez@gmail.com>
2022-05-25 23:41:02 +0200

    refactor(iocuak): add GetInstanceDependenciesTaskGraphExpandCommand

---
+++ packages/iocuak/src/createInstanceTask/models/cuaktask/GetInstanceDependenciesTaskGraphExpandCommand.ts
@@ -0,0 +1,12 @@
+import { Node, Task } from '@cuaklabs/cuaktask';
+
+import { GraphExpandCommand } from '../../../common/models/cuaktask/GraphExpandCommand';
+import { GetInstanceDependenciesTaskKind } from '../domain/GetInstanceDependenciesTaskKind';
+import { TaskKind } from '../domain/TaskKind';
+import { TaskKindType } from '../domain/TaskKindType';
+
+export type GetInstanceDependenciesTaskGraphExpandCommand = GraphExpandCommand<
+  TaskKindType.getInstanceDependencies,
+  Node<Task<TaskKind>>,
+  Node<Task<GetInstanceDependenciesTaskKind>>
+>;
