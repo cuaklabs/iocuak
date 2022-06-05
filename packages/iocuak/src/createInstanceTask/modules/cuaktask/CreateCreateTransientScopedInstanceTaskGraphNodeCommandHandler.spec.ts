98498de9ba1de5d9bc9129a99f56cd5633fb34f2
notaphplover <roberto.pintos.lopez@gmail.com>
2022-06-05 22:17:09 +0200

    refactor(iocuak): add CreateCreateTransientScopedInstanceTaskGraphNodeCommandHandler

---
+++ packages/iocuak/src/createInstanceTask/modules/cuaktask/CreateCreateTransientScopedInstanceTaskGraphNodeCommandHandler.spec.ts
@@ -0,0 +1,104 @@
+import * as cuaktask from '@cuaklabs/cuaktask';
+
+import { Handler } from '../../../common/modules/domain/Handler';
+import { ContainerRequestService } from '../../../container/services/domain/ContainerRequestService';
+import { ContainerSingletonService } from '../../../container/services/domain/ContainerSingletonService';
+import { CreateInstanceTaskKindFixtures } from '../../fixtures/domain/CreateInstanceTaskKindFixtures';
+import { CreateCreateInstanceTaskGraphNodeCommand } from '../../models/cuaktask/CreateCreateInstanceTaskGraphNodeCommand';
+import { CreateInstanceTask } from '../../models/cuaktask/CreateInstanceTask';
+import { CreateInstanceTaskGraphExpandCommand } from '../../models/cuaktask/CreateInstanceTaskGraphExpandCommand';
+import { CreateInstanceTaskKind } from '../../models/domain/CreateInstanceTaskKind';
+import { TaskKindType } from '../../models/domain/TaskKindType';
+import { CreateCreateTransientScopedInstanceTaskGraphNodeCommandHandler } from './CreateCreateTransientScopedInstanceTaskGraphNodeCommandHandler';
+
+describe(
+  CreateCreateTransientScopedInstanceTaskGraphNodeCommandHandler.name,
+  () => {
+    let busMock: jest.Mocked<Handler<unknown, void | Promise<void>>>;
+    let containerRequestServiceFixture: ContainerRequestService;
+    let containerSingletonServiceFixture: ContainerSingletonService;
+
+    let createCreateTransientScopedInstanceTaskGraphNodeCommandHandler: CreateCreateTransientScopedInstanceTaskGraphNodeCommandHandler;
+
+    beforeAll(() => {
+      busMock = {
+        handle: jest.fn(),
+      };
+      containerRequestServiceFixture = {
+        _type: Symbol(),
+      } as unknown as ContainerRequestService;
+      containerSingletonServiceFixture = {
+        _type: Symbol(),
+      } as unknown as ContainerSingletonService;
+
+      createCreateTransientScopedInstanceTaskGraphNodeCommandHandler =
+        new CreateCreateTransientScopedInstanceTaskGraphNodeCommandHandler(
+          busMock,
+          containerRequestServiceFixture,
+          containerSingletonServiceFixture,
+        );
+    });
+
+    describe('.handle', () => {
+      describe('when called', () => {
+        let createInstanceTaskGraphFromTypeBindingTaskKindExpandCommand: CreateCreateInstanceTaskGraphNodeCommand;
+        let createInstanceTaskKindGraphNode: cuaktask.Node<
+          cuaktask.Task<CreateInstanceTaskKind>
+        >;
+        let result: unknown;
+
+        beforeAll(() => {
+          createInstanceTaskGraphFromTypeBindingTaskKindExpandCommand = {
+            context: {
+              graph: {
+                nodes: [],
+              },
+              serviceIdToRequestCreateInstanceTaskKindNode: new Map(),
+              serviceIdToSingletonCreateInstanceTaskKindNode: new Map(),
+              taskKind: CreateInstanceTaskKindFixtures.withBindingType,
+            },
+          };
+
+          createInstanceTaskKindGraphNode = {
+            dependencies: undefined,
+            element: new CreateInstanceTask(
+              createInstanceTaskGraphFromTypeBindingTaskKindExpandCommand.context.taskKind,
+              containerRequestServiceFixture,
+              containerSingletonServiceFixture,
+            ),
+          };
+
+          busMock.handle.mockReturnValueOnce(undefined);
+
+          result =
+            createCreateTransientScopedInstanceTaskGraphNodeCommandHandler.handle(
+              createInstanceTaskGraphFromTypeBindingTaskKindExpandCommand,
+            );
+        });
+
+        afterAll(() => {
+          jest.clearAllMocks();
+        });
+
+        it('should call bus.handle()', () => {
+          const createInstanceTaskGraphExpandCommand: CreateInstanceTaskGraphExpandCommand =
+            {
+              context:
+                createInstanceTaskGraphFromTypeBindingTaskKindExpandCommand.context,
+              node: createInstanceTaskKindGraphNode,
+              taskKindType: TaskKindType.createInstance,
+            };
+
+          expect(busMock.handle).toHaveBeenCalledTimes(1);
+          expect(busMock.handle).toHaveBeenCalledWith(
+            createInstanceTaskGraphExpandCommand,
+          );
+        });
+
+        it('should return a NodeDependency', () => {
+          expect(result).toStrictEqual(createInstanceTaskKindGraphNode);
+        });
+      });
+    });
+  },
+);
---
+++ packages/iocuak/src/createInstanceTask/modules/cuaktask/CreateCreateTransientScopedInstanceTaskGraphNodeCommandHandler.ts
@@ -0,0 +1,81 @@
+import * as cuaktask from '@cuaklabs/cuaktask';
+
+import { TypeBinding } from '../../../binding/models/domain/TypeBinding';
+import { Handler } from '../../../common/modules/domain/Handler';
+import { ContainerRequestService } from '../../../container/services/domain/ContainerRequestService';
+import { ContainerSingletonService } from '../../../container/services/domain/ContainerSingletonService';
+import { CreateCreateInstanceTaskGraphNodeCommand } from '../../models/cuaktask/CreateCreateInstanceTaskGraphNodeCommand';
+import { CreateInstanceTask } from '../../models/cuaktask/CreateInstanceTask';
+import { CreateInstanceTaskGraphExpandCommand } from '../../models/cuaktask/CreateInstanceTaskGraphExpandCommand';
+import { CreateInstanceTaskGraphFromTaskKindExpandOperationContext } from '../../models/cuaktask/CreateInstanceTaskGraphFromTaskKindExpandOperationContext';
+import { CreateInstanceTaskKind } from '../../models/domain/CreateInstanceTaskKind';
+import { TaskKind } from '../../models/domain/TaskKind';
+import { TaskKindType } from '../../models/domain/TaskKindType';
+
+export class CreateCreateTransientScopedInstanceTaskGraphNodeCommandHandler
+  implements
+    Handler<
+      CreateCreateInstanceTaskGraphNodeCommand,
+      cuaktask.NodeDependency<cuaktask.Task<TaskKind>>
+    >
+{
+  readonly #bus: Handler<unknown, void | Promise<void>>;
+  readonly #containerRequestService: ContainerRequestService;
+  readonly #containerSingletonService: ContainerSingletonService;
+
+  constructor(
+    bus: Handler<unknown, void | Promise<void>>,
+    containerRequestService: ContainerRequestService,
+    containerSingletonService: ContainerSingletonService,
+  ) {
+    this.#bus = bus;
+    this.#containerRequestService = containerRequestService;
+    this.#containerSingletonService = containerSingletonService;
+  }
+
+  public handle(
+    createInstanceTaskGraphFromTypeBindingTaskKindExpandCommand: CreateCreateInstanceTaskGraphNodeCommand,
+  ): cuaktask.NodeDependency<cuaktask.Task<TaskKind, unknown[], unknown>> {
+    const createInstanceTaskKindGraphNode: cuaktask.Node<
+      cuaktask.Task<CreateInstanceTaskKind>
+    > = this.#createNewCreateInstanceTaskKindGraphNode(
+      createInstanceTaskGraphFromTypeBindingTaskKindExpandCommand.context,
+    );
+
+    return createInstanceTaskKindGraphNode;
+  }
+
+  #createNewCreateInstanceTaskKindGraphNode(
+    context: CreateInstanceTaskGraphFromTaskKindExpandOperationContext<
+      CreateInstanceTaskKind<TypeBinding>
+    >,
+  ): cuaktask.Node<cuaktask.Task<CreateInstanceTaskKind>> {
+    const createInstanceTaskKindGraphNode: cuaktask.Node<
+      cuaktask.Task<CreateInstanceTaskKind>
+    > = {
+      dependencies: undefined,
+      element: new CreateInstanceTask(
+        context.taskKind,
+        this.#containerRequestService,
+        this.#containerSingletonService,
+      ),
+    };
+
+    const createInstanceTaskGraphExpandCommand: CreateInstanceTaskGraphExpandCommand =
+      {
+        context: context,
+        node: createInstanceTaskKindGraphNode,
+        taskKindType: TaskKindType.createInstance,
+      };
+
+    const result: void | Promise<void> = this.#bus.handle(
+      createInstanceTaskGraphExpandCommand,
+    );
+
+    if (cuaktask.isPromiseLike(result)) {
+      throw new Error('Expected syncronous flow');
+    }
+
+    return createInstanceTaskKindGraphNode;
+  }
+}
