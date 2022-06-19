9850ccd33b346f52bd8c3323d2d4648e045a0860
notaphplover <roberto.pintos.lopez@gmail.com>
2022-06-19 20:34:21 +0200

    test(iocuak): update type service definitions
    update type service definition with additional type services

---
+++ packages/iocuak/src/e2e/definitions/common/parameters/typeService/getTypeServiceWithBindingWithRequestScopeAndDependenciesWithTransientScope.ts
@@ -0,0 +1,48 @@
+import sinon from 'sinon';
+
+import { injectable } from '../../../../../binding/decorators/injectable';
+import { BindingScopeApi } from '../../../../../binding/models/api/BindingScopeApi';
+import { BindingTypeApi } from '../../../../../binding/models/api/BindingTypeApi';
+import { TypeBindingApi } from '../../../../../binding/models/api/TypeBindingApi';
+import { inject } from '../../../../../classMetadata/decorators/inject';
+import { getTypeServiceWithBindingWithTransientScope } from './getTypeServiceWithBindingWithTransientScope';
+import { TypeServiceParameter } from './TypeServiceParameter';
+
+export function getTypeServiceWithBindingWithRequestScopeAndDependenciesWithTransientScope(): TypeServiceParameter {
+  // eslint-disable-next-line import/no-named-as-default-member
+  const constructorSpy: sinon.SinonSpy = sinon.spy();
+
+  const typeServiceWithTransientScopeParameter: TypeServiceParameter =
+    getTypeServiceWithBindingWithTransientScope();
+
+  class TypeServiceWithBindingWithRequestScopeAndDependenciesWithTransientScope {
+    constructor(
+      @inject(typeServiceWithTransientScopeParameter.binding.id)
+      public typeServiceInstance: unknown,
+    ) {
+      constructorSpy(typeServiceInstance);
+    }
+  }
+
+  const binding: TypeBindingApi = {
+    bindingType: BindingTypeApi.type,
+    id: Symbol(
+      TypeServiceWithBindingWithRequestScopeAndDependenciesWithTransientScope.name,
+    ),
+    scope: BindingScopeApi.request,
+    type: TypeServiceWithBindingWithRequestScopeAndDependenciesWithTransientScope,
+  };
+
+  injectable({
+    id: binding.id,
+    scope: binding.scope,
+  })(TypeServiceWithBindingWithRequestScopeAndDependenciesWithTransientScope);
+
+  return {
+    binding,
+    dependencies: [typeServiceWithTransientScopeParameter],
+    service:
+      TypeServiceWithBindingWithRequestScopeAndDependenciesWithTransientScope,
+    spy: constructorSpy,
+  };
+}
---
+++ packages/iocuak/src/e2e/definitions/common/parameters/typeService/getTypeServiceWithTwoDependenciesOfTheSameTypeWithTransientScopeAndDependenciesWithTransientScope.ts
@@ -0,0 +1,52 @@
+import sinon from 'sinon';
+
+import { injectable } from '../../../../../binding/decorators/injectable';
+import { BindingScopeApi } from '../../../../../binding/models/api/BindingScopeApi';
+import { BindingTypeApi } from '../../../../../binding/models/api/BindingTypeApi';
+import { TypeBindingApi } from '../../../../../binding/models/api/TypeBindingApi';
+import { inject } from '../../../../../classMetadata/decorators/inject';
+import { getTypeServiceWithBindingWithTransientScopeAndDependenciesWithTransientScope } from './getTypeServiceWithBindingWithTransientScopeAndDependenciesWithTransientScope';
+import { TypeServiceParameter } from './TypeServiceParameter';
+
+export function getTypeServiceWithTwoDependenciesOfTheSameTypeWithTransientScopeAndDependenciesWithTransientScope(): TypeServiceParameter {
+  // eslint-disable-next-line import/no-named-as-default-member
+  const constructorSpy: sinon.SinonSpy = sinon.spy();
+
+  const typeServiceWithTransientScopeParameter: TypeServiceParameter =
+    getTypeServiceWithBindingWithTransientScopeAndDependenciesWithTransientScope();
+
+  class TypeServiceWithTwoDependenciesOfTheSameTypeWithTransientScopeAndDependenciesWithTransientScope {
+    constructor(
+      @inject(typeServiceWithTransientScopeParameter.binding.id)
+      public firstTypeServiceInstance: unknown,
+      @inject(typeServiceWithTransientScopeParameter.binding.id)
+      public secondTypeServiceInstance: unknown,
+    ) {
+      constructorSpy(firstTypeServiceInstance, secondTypeServiceInstance);
+    }
+  }
+
+  const binding: TypeBindingApi = {
+    bindingType: BindingTypeApi.type,
+    id: Symbol(
+      TypeServiceWithTwoDependenciesOfTheSameTypeWithTransientScopeAndDependenciesWithTransientScope.name,
+    ),
+    scope: BindingScopeApi.singleton,
+    type: TypeServiceWithTwoDependenciesOfTheSameTypeWithTransientScopeAndDependenciesWithTransientScope,
+  };
+
+  injectable({
+    id: binding.id,
+    scope: binding.scope,
+  })(
+    TypeServiceWithTwoDependenciesOfTheSameTypeWithTransientScopeAndDependenciesWithTransientScope,
+  );
+
+  return {
+    binding,
+    dependencies: [typeServiceWithTransientScopeParameter],
+    service:
+      TypeServiceWithTwoDependenciesOfTheSameTypeWithTransientScopeAndDependenciesWithTransientScope,
+    spy: constructorSpy,
+  };
+}
