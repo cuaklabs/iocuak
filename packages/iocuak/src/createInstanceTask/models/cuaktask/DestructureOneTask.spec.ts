f180219b50d393aa70e0d4394d03584a19ffe7e2
notaphplover <roberto.pintos.lopez@gmail.com>
2022-06-20 00:29:57 +0200

    refactor(iocuak): add DestructureOneTask

---
+++ packages/iocuak/src/createInstanceTask/models/cuaktask/DestructureOneTask.spec.ts
@@ -0,0 +1,38 @@
+import jest from '@jest/globals';
+
+import { DestructureOneTaskKind } from '../domain/DestructureOneTaskKind';
+import { TaskKindType } from '../domain/TaskKindType';
+import { DestructureOneTask } from './DestructureOneTask';
+
+jest.describe(DestructureOneTask.name, () => {
+  let destructureOneTaskKind: DestructureOneTaskKind;
+
+  jest.beforeAll(() => {
+    destructureOneTaskKind = {
+      requestId: Symbol(),
+      type: TaskKindType.destructureOne,
+    };
+  });
+
+  jest.describe('.perform', () => {
+    jest.describe('when called', () => {
+      let destructureOneTask: DestructureOneTask;
+
+      let paramFixture: [unknown];
+
+      let result: unknown;
+
+      jest.beforeAll(() => {
+        destructureOneTask = new DestructureOneTask(destructureOneTaskKind);
+
+        paramFixture = [Symbol()];
+
+        result = destructureOneTask.perform(paramFixture);
+      });
+
+      it('should return the destructure value', () => {
+        expect([result]).toStrictEqual(paramFixture);
+      });
+    });
+  });
+});
