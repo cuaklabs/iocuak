420e74d5fd424af79cf19b1e527e8a7d0f50f217
notaphplover <roberto.pintos.lopez@gmail.com>
2022-06-19 22:48:01 +0200

    refactor(iocuak): update TaskKind with DestructureOneTaskKind

--- packages/iocuak/src/createInstanceTask/models/domain/TaskKind.ts
+++ packages/iocuak/src/createInstanceTask/models/domain/TaskKind.ts
@@ -1,10 +1,12 @@
 import { CreateInstanceRootTaskKind } from './CreateInstanceRootTaskKind';
 import { CreateInstanceTaskKind } from './CreateInstanceTaskKind';
+import { DestructureOneTaskKind } from './DestructureOneTaskKind';
 import { GetCachedInstanceTaskKind } from './GetCachedInstanceTaskKind';
 import { GetInstanceDependenciesTaskKind } from './GetInstanceDependenciesTaskKind';
 
 export type TaskKind =
   | CreateInstanceTaskKind
   | CreateInstanceRootTaskKind
+  | DestructureOneTaskKind
   | GetCachedInstanceTaskKind
   | GetInstanceDependenciesTaskKind;
