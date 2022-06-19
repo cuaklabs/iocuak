ed788a2c8c8aed8685b95fb94ffd69c7ef91b30e
notaphplover <roberto.pintos.lopez@gmail.com>
2022-06-19 22:44:14 +0200

    refactor(iocuak): update TaskKindType with destructureOne

--- packages/iocuak/src/createInstanceTask/models/domain/TaskKindType.ts
+++ packages/iocuak/src/createInstanceTask/models/domain/TaskKindType.ts
@@ -2,6 +2,7 @@ export enum TaskKindType {
   createArray = 'createArray',
   createInstance = 'createInstance',
   createInstanceRoot = 'createInstanceRoot',
+  destructureOne = 'destructureOne',
   getCachedInstance = 'getCachedInstance',
   getInstanceDependencies = 'getInstanceDependencies',
 }
