2cf7f9d24a1466f066bfc6d2cceae35e7b21415d
notaphplover <roberto.pintos.lopez@gmail.com>
2022-06-21 21:47:40 +0200

    refactor(iocuak): update InjectableOptionsApi with tags

--- packages/iocuak/src/binding/models/api/InjectableOptionsApi.ts
+++ packages/iocuak/src/binding/models/api/InjectableOptionsApi.ts
@@ -1,7 +1,9 @@
 import { ServiceId } from '../../../common/models/domain/ServiceId';
+import { BindingTag } from '../domain/BindingTag';
 import { BindingScopeApi } from './BindingScopeApi';
 
 export interface InjectableOptionsApi {
   id?: ServiceId;
   scope?: BindingScopeApi;
+  tags?: BindingTag | BindingTag[];
 }
