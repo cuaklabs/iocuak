9e7f615b91a03238f82b644c09813778c29802e2
notaphplover <roberto.pintos.lopez@gmail.com>
2022-06-21 15:22:11 +0200

    feat(iocuak): update ClassMetadataApi to have ClassElementMetadataApi based fields

--- packages/iocuak/src/classMetadata/utils/api/convertToClassMetadataApi.ts
+++ packages/iocuak/src/classMetadata/utils/api/convertToClassMetadataApi.ts
@@ -1,11 +1,39 @@
+import { ServiceId } from '../../../common/models/domain/ServiceId';
+import { mapIterable } from '../../../common/utils/mapIterable';
+import { ClassElementMetadataApi } from '../../models/api/ClassElementMetadataApi';
+import { ClassElementMetadatApiType } from '../../models/api/ClassElementMetadatApiType';
 import { ClassMetadataApi } from '../../models/api/ClassMetadataApi';
 import { ClassMetadata } from '../../models/domain/ClassMetadata';
 
 export function convertToClassMetadataApi(
   classMetadata: ClassMetadata,
 ): ClassMetadataApi {
+  const classMetadataApiConstructorArguments: ClassElementMetadataApi[] =
+    classMetadata.constructorArguments.map(serviceIdToClassMetadataApi);
+  const classMetadataApiProperties: Map<
+    string | symbol,
+    ClassElementMetadataApi
+  > = new Map(
+    mapIterable(
+      classMetadata.properties.entries(),
+      ([key, serviceId]: [string | symbol, ServiceId]) => [
+        key,
+        serviceIdToClassMetadataApi(serviceId),
+      ],
+    ),
+  );
+
   return {
-    constructorArguments: [...classMetadata.constructorArguments],
-    properties: new Map(classMetadata.properties),
+    constructorArguments: classMetadataApiConstructorArguments,
+    properties: classMetadataApiProperties,
+  };
+}
+
+function serviceIdToClassMetadataApi(
+  serviceId: ServiceId,
+): ClassElementMetadataApi {
+  return {
+    type: ClassElementMetadatApiType.serviceId,
+    value: serviceId,
   };
 }
