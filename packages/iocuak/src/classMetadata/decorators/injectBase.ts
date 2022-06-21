e058acc89594fa90085361c093bf6b9364745501
notaphplover <roberto.pintos.lopez@gmail.com>
2022-06-22 00:22:35 +0200

    refactor(iocuak): add injectBase

---
+++ packages/iocuak/src/classMetadata/decorators/injectBase.ts
@@ -0,0 +1,94 @@
+import { MetadataKey } from '../../reflectMetadata/models/domain/MetadataKey';
+import { updateReflectMetadata } from '../../reflectMetadata/utils/domain/updateReflectMetadata';
+import { ClassElementMetadata } from '../models/domain/ClassElementMetadata';
+import { ClassMetadata } from '../models/domain/ClassMetadata';
+import { getDefaultClassMetadata } from '../utils/domain/getDefaultClassMetadata';
+
+export function injectBase<TInput>(
+  input: TInput,
+  inputToClassElementMetadata: (input: TInput) => ClassElementMetadata,
+): ParameterDecorator & PropertyDecorator {
+  const decorator: ParameterDecorator & PropertyDecorator = (
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    target: Object,
+    propertyKey: string | symbol, // It may be undefined!!
+    parameterIndex?: number,
+  ): void => {
+    if (parameterIndex === undefined) {
+      return injectProperty(input, inputToClassElementMetadata)(
+        target,
+        propertyKey,
+      );
+    } else {
+      return injectParameter(input, inputToClassElementMetadata)(
+        target,
+        propertyKey,
+        parameterIndex,
+      );
+    }
+  };
+
+  return decorator;
+}
+
+function injectParameter<TInput>(
+  input: TInput,
+  inputToClassElementMetadata: (input: TInput) => ClassElementMetadata,
+): ParameterDecorator {
+  return (
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    target: Object,
+    propertyKey: string | symbol | undefined,
+    parameterIndex: number,
+  ): void => {
+    if (isConstructorParameter(target, propertyKey)) {
+      updateReflectMetadata(
+        target,
+        MetadataKey.inject,
+        getDefaultClassMetadata(),
+        (classMetadata: ClassMetadata): ClassMetadata => {
+          classMetadata.constructorArguments[parameterIndex] =
+            inputToClassElementMetadata(input);
+
+          return classMetadata;
+        },
+      );
+    } else {
+      throw new Error(
+        `Found an @inject decorator in a non constructor parameter.
+Found @inject decorator at method "${
+          propertyKey?.toString() ?? ''
+        }" at class "${target.constructor.name}"`,
+      );
+    }
+  };
+}
+
+function injectProperty<TInput>(
+  input: TInput,
+  inputToClassElementMetadata: (input: TInput) => ClassElementMetadata,
+): PropertyDecorator {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return (target: Object, propertyKey: string | symbol): void => {
+    updateReflectMetadata(
+      target.constructor,
+      MetadataKey.inject,
+      getDefaultClassMetadata(),
+      (classMetadata: ClassMetadata): ClassMetadata => {
+        classMetadata.properties.set(
+          propertyKey,
+          inputToClassElementMetadata(input),
+        );
+
+        return classMetadata;
+      },
+    );
+  };
+}
+
+function isConstructorParameter(
+  target: unknown,
+  propertyKey: string | symbol | undefined,
+): boolean {
+  return typeof target === 'function' && propertyKey === undefined;
+}
