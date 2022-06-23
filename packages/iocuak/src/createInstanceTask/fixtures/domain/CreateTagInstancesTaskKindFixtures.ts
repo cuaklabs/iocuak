ab9c280e6c15e1f8dde47a53bbf9090deb4701f5
notaphplover <roberto.pintos.lopez@gmail.com>
2022-06-23 20:02:06 +0200

    refactor(iocuak): add CreateTagInstancesTask

---
+++ packages/iocuak/src/createInstanceTask/fixtures/domain/CreateTagInstancesTaskKindFixtures.ts
@@ -0,0 +1,14 @@
+import { CreateTagInstancesTaskKind } from '../../models/domain/CreateTagInstancesTaskKind';
+import { TaskKindType } from '../../models/domain/TaskKindType';
+
+export class CreateTagInstancesTaskKindFixtures {
+  public static get any(): CreateTagInstancesTaskKind {
+    const fixture: CreateTagInstancesTaskKind = {
+      requestId: Symbol(),
+      tag: Symbol(),
+      type: TaskKindType.createTagInstances,
+    };
+
+    return fixture;
+  }
+}
---
+++ packages/iocuak/src/createInstanceTask/models/cuaktask/CreateTagInstancesTask.spec.ts
@@ -0,0 +1,28 @@
+import { CreateTagInstancesTaskKindFixtures } from '../../fixtures/domain/CreateTagInstancesTaskKindFixtures';
+import { CreateTagInstancesTask } from './CreateTagInstancesTask';
+
+describe(CreateTagInstancesTask.name, () => {
+  let createTagInstancesTask: CreateTagInstancesTask;
+
+  beforeAll(() => {
+    createTagInstancesTask = new CreateTagInstancesTask(
+      CreateTagInstancesTaskKindFixtures.any,
+    );
+  });
+
+  describe('when called', () => {
+    let servicesFixture: unknown[];
+
+    let result: unknown;
+
+    beforeAll(() => {
+      servicesFixture = [Symbol()];
+
+      result = createTagInstancesTask.perform(...servicesFixture);
+    });
+
+    it('should return an array', () => {
+      expect(result).toStrictEqual(servicesFixture);
+    });
+  });
+});
