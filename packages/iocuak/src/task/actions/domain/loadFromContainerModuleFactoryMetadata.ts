dd77210a3831c7177d1e5c12b961edb052435cee
notaphplover <roberto.pintos.lopez@gmail.com>
2022-07-06 18:15:14 +0200

    refactor(iocuak-benchmark): add loadFromContainerModuleFactoryMetadata

---
+++ packages/iocuak/src/task/actions/domain/loadFromContainerModuleFactoryMetadata.ts
@@ -0,0 +1,43 @@
+import * as cuaktask from '@cuaklabs/cuaktask';
+
+import { ServiceId } from '../../../common/models/domain/ServiceId';
+import { ContainerModule } from '../../../containerModule/models/domain/ContainerModule';
+import { ContainerModuleFactoryMetadata } from '../../../containerModuleMetadata/models/domain/ContainerModuleFactoryMetadata';
+import { TaskContext } from '../../models/domain/TaskContext';
+import { createInstance } from './createInstance';
+
+export function loadFromContainerModuleFactoryMetadata(
+  metadata: ContainerModuleFactoryMetadata,
+  context: TaskContext,
+): void | Promise<void> {
+  const instances: unknown[] = metadata.injects.map((inject: ServiceId) =>
+    createInstance(inject, {
+      ...context,
+      servicesInstantiatedSet: new Set(),
+    }),
+  );
+
+  const containerModule: ContainerModule | Promise<ContainerModule> =
+    metadata.factory(...instances);
+
+  if (cuaktask.isPromiseLike(containerModule)) {
+    return loadModuleAsync(containerModule, context);
+  } else {
+    containerModule.load(
+      context.services.bindingService,
+      context.services.metadataService,
+    );
+  }
+}
+
+async function loadModuleAsync(
+  containerModulePromise: Promise<ContainerModule>,
+  context: TaskContext,
+): Promise<void> {
+  const containerModule: ContainerModule = await containerModulePromise;
+
+  containerModule.load(
+    context.services.bindingService,
+    context.services.metadataService,
+  );
+}
