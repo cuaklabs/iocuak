28d70b61760e28f7dc9513068f5933d25e948265
notaphplover <roberto.pintos.lopez@gmail.com>
2022-08-06 13:11:16 +0200

    refactor(iocuak-core): add stringifyServiceId

---
+++ packages/iocuak-core/src/common/utils/domain/stringifyServiceId.ts
@@ -0,0 +1,13 @@
+import { ServiceId } from '@cuaklabs/iocuak-common';
+
+export function stringifyServiceId(serviceId: ServiceId): string {
+  switch (typeof serviceId) {
+    case 'string':
+    case 'symbol':
+      return serviceId.toString();
+    case 'function':
+      return serviceId.name;
+    default:
+      throw new Error(`Unexpected ${typeof serviceId} service id type`);
+  }
+}
