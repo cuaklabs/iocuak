d22bb1474f65c1b5755c7894daea952d98e59d6a
notaphplover <roberto.pintos.lopez@gmail.com>
2022-08-09 22:54:23 +0200

    refactor(iocuak): remove unused fixtures

--- packages/iocuak/src/binding/fixtures/domain/TypeBindingFixtures.ts
+++ packages/iocuak/src/binding/fixtures/domain/TypeBindingFixtures.ts
@@ -1,11 +1,9 @@
-import { Newable, Tag } from '@cuaklabs/iocuak-common';
+import { Newable } from '@cuaklabs/iocuak-common';
 import {
   TypeBinding,
   BindingScope,
   BindingType,
 } from '@cuaklabs/iocuak-models';
-
-import { PickKeys } from '../../../common/models/domain/PickKeys';
 
 export class TypeBindingFixtures {
   static readonly #type: Newable = class {};
@@ -17,46 +15,6 @@ export class TypeBindingFixtures {
       scope: BindingScope.request,
       tags: [],
       type: TypeBindingFixtures.#type,
-    };
-
-    return fixture;
-  }
-
-  public static get withTagsOne(): TypeBinding & {
-    [T in PickKeys<TypeBinding, 'tags'>]: [Tag] & Tag[];
-  } {
-    const fixture: TypeBinding & {
-      [T in PickKeys<TypeBinding, 'tags'>]: [Tag] & Tag[];
-    } = {
-      ...TypeBindingFixtures.any,
-      tags: ['tag'],
-    };
-
-    return fixture;
-  }
-
-  public static get withScopeRequest(): TypeBinding {
-    const fixture: TypeBinding = {
-      ...TypeBindingFixtures.any,
-      scope: BindingScope.request,
-    };
-
-    return fixture;
-  }
-
-  public static get withScopeSingleton(): TypeBinding {
-    const fixture: TypeBinding = {
-      ...TypeBindingFixtures.any,
-      scope: BindingScope.singleton,
-    };
-
-    return fixture;
-  }
-
-  public static get withScopeTransient(): TypeBinding {
-    const fixture: TypeBinding = {
-      ...TypeBindingFixtures.any,
-      scope: BindingScope.transient,
     };
 
     return fixture;
