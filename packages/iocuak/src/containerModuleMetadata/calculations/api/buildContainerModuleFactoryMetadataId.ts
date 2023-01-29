797d7f59cfea954675938feb8c5848f2807513d6
notaphplover <roberto.pintos.lopez@gmail.com>
2023-01-29 23:33:16 +0100

    refactor(iocuak): add buildContainerModuleFactoryMetadataId

---
+++ packages/iocuak/src/containerModuleMetadata/calculations/api/buildContainerModuleFactoryMetadataId.ts
@@ -0,0 +1,49 @@
+import {
+  ContainerModuleMetadataId,
+  ServiceId,
+  stringifyServiceId,
+} from '@cuaklabs/iocuak-common';
+import {
+  ClassElementMetadataApi,
+  ClassElementMetadataApiType,
+} from '@cuaklabs/iocuak-models-api';
+
+import { isClassElementMetadataApi } from '../../../classMetadata/utils/api/isClassElementMetadataApi';
+import { hashString } from '../../../foundation/calculations/hashString';
+import { ContainerModuleFactoryMetadataApi } from '../../models/api/ContainerModuleFactoryMetadataApi';
+
+export function buildContainerModuleFactoryMetadataId<TArgs extends unknown[]>(
+  metadata: ContainerModuleFactoryMetadataApi<TArgs>,
+): ContainerModuleMetadataId {
+  const stringifiedMetadata: string = `${stringifyInjects(
+    metadata.injects,
+  )}|${metadata.factory.toString()}`;
+
+  return hashString(stringifiedMetadata);
+}
+
+function stringifyInjects(
+  injects: (ServiceId | ClassElementMetadataApi)[] | undefined,
+): string {
+  return injects?.map(stringifyInject).join(',') ?? '';
+}
+
+function stringifyInject(inject: ServiceId | ClassElementMetadataApi): string {
+  let stringifiedInject: string;
+
+  if (isClassElementMetadataApi(inject)) {
+    stringifiedInject = `${inject.type}|`;
+    switch (inject.type) {
+      case ClassElementMetadataApiType.serviceId:
+        stringifiedInject += stringifyServiceId(inject.value);
+        break;
+      case ClassElementMetadataApiType.tag:
+        stringifiedInject += inject.value.toString();
+        break;
+    }
+  } else {
+    stringifiedInject = stringifyServiceId(inject);
+  }
+
+  return stringifiedInject;
+}
