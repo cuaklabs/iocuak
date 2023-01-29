872c44b9d1d1761af19079cccc28d454f5dd4939
notaphplover <roberto.pintos.lopez@gmail.com>
2023-01-29 23:31:59 +0100

    refactor(iocuak): add buildContainerModuleClassMetadataId

---
+++ packages/iocuak/src/containerModuleMetadata/calculations/api/buildContainerModuleClassMetadataId.spec.ts
@@ -0,0 +1,26 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+
+import { ContainerModuleMetadataApiMocks } from '../../mocks/models/api/ContainerModuleMetadataApiMocks';
+import { ContainerModuleClassMetadataApi } from '../../models/api/ContainerModuleClassMetadataApi';
+import { buildContainerModuleClassMetadataId } from './buildContainerModuleClassMetadataId';
+
+describe(buildContainerModuleClassMetadataId.name, () => {
+  let metadataFixture: ContainerModuleClassMetadataApi;
+
+  beforeAll(() => {
+    metadataFixture =
+      ContainerModuleMetadataApiMocks.anyContainerModuleClassMetadataApi;
+  });
+
+  describe('when called', () => {
+    let result: unknown;
+
+    beforeAll(() => {
+      result = buildContainerModuleClassMetadataId(metadataFixture);
+    });
+
+    it('should return a ContainerModuleMetadataId', () => {
+      expect(result).toBe(metadataFixture.module);
+    });
+  });
+});
