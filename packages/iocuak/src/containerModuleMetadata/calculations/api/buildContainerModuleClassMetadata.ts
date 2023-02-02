b781e785a34228a0c2cd2f0e70730db5209f2ea2
notaphplover <roberto.pintos.lopez@gmail.com>
2023-02-02 14:45:26 +0100

    refactor(iocuak): add buildContainerModuleClassMetadata

---
+++ packages/iocuak/src/containerModuleMetadata/calculations/api/buildContainerModuleClassMetadata.ts
@@ -0,0 +1,37 @@
+import { ContainerModuleMetadataId } from '@cuaklabs/iocuak-common';
+import {
+  BindingService,
+  ContainerModule,
+  ContainerModuleClassMetadata,
+  ContainerModuleMetadataType,
+} from '@cuaklabs/iocuak-core';
+
+import { ContainerModuleApi } from '../../../containerModule/models/api/ContainerModuleApi';
+import { convertToContainerModule } from '../../../containerModule/utils/api/convertToContainerModule';
+import { ContainerModuleClassMetadataApi } from '../../models/api/ContainerModuleClassMetadataApi';
+import { buildContainerModuleClassMetadataId } from './buildContainerModuleClassMetadataId';
+
+export function buildContainerModuleClassMetadata(
+  containerModuleClassMetadataApi: ContainerModuleClassMetadataApi,
+  requires: ContainerModuleMetadataId[],
+): ContainerModuleClassMetadata {
+  const containerModuleClassMetadata: ContainerModuleClassMetadata<ContainerModuleApi> =
+    {
+      id: buildContainerModuleClassMetadataId(containerModuleClassMetadataApi),
+      imports: [],
+      loader: (
+        containerModuleApi: ContainerModuleApi,
+        bindingService: BindingService,
+      ) => {
+        const containerModule: ContainerModule =
+          convertToContainerModule(containerModuleApi);
+
+        containerModule.load(bindingService);
+      },
+      moduleType: containerModuleClassMetadataApi.module,
+      requires,
+      type: ContainerModuleMetadataType.clazz,
+    };
+
+  return containerModuleClassMetadata as ContainerModuleClassMetadata;
+}
