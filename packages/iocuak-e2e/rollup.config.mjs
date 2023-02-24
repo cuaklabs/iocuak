7629a66493d5825e0e47150a10eae9d0ba22011f
notaphplover <roberto.pintos.lopez@gmail.com>
2023-02-24 09:57:50 +0100

    chore(rollup): add onwarn handler to supress wrong warnings

--- packages/iocuak-e2e/rollup.config.mjs
+++ packages/iocuak-e2e/rollup.config.mjs
@@ -1,6 +1,17 @@
 import multi from '@rollup/plugin-multi-entry';
 import typescript from '@rollup/plugin-typescript';
 
+const TS_LIB_WARN_MESSAGE =
+  "@rollup/plugin-typescript TS2354: This syntax requires an imported helper but module 'tslib' cannot be found.";
+
+/**
+ * @param {!import("rollup").RollupLog} warning
+ * @returns boolean
+ */
+function isTsLibRequiredWarning(warning) {
+  return warning.message === TS_LIB_WARN_MESSAGE;
+}
+
 /** @type {!import("rollup").MergedRollupOptions[]} */
 export default [
   {
@@ -12,6 +23,11 @@ export default [
       'sinon-chai',
     ],
     input: './src/**/*.ts',
+    onwarn: (warning, defaultHandler) => {
+      if (!isTsLibRequiredWarning(warning)) {
+        defaultHandler(warning);
+      }
+    },
     output: [
       {
         dir: './lib/esm',
